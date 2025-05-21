1e49b921c059d7add1db00046da0a08dd0ab0337
Luckas33 <sobrallucas33@gmail.com>
2025-05-21 00:08:24 -0300

    Secante + newton

--- 2-Metodos_abertos/2.2-MetodoNewton-Raphson/Formula/Newton-Raphson.py
+++ 2-Metodos_abertos/2.2-MetodoNewton-Raphson/Formula/Newton-Raphson.py
@@ -1,8 +1,21 @@
 import math
 import numpy as np
 
+def iteracao(f,df, x):
+    return x - (f(x)/(df(x)))
 
-def Newton_Raphson():
+def Newton_Raphson(x0, f, df ,Tol, maxIter):
+    iter = 1
     
+    x_atual = iteracao(f, df, x0)
     
-    return
+    erro = np.abs((x_atual - x0) / x_atual) * 100
+    
+    while (erro > Tol and iter < maxIter):
+        x_anterior = x_atual
+        x_atual = iteracao(f, df, x_anterior)
+        
+        erro = np.abs((x_atual - x_anterior) / x_atual) * 100
+        iter += 1
+    
+    return x_atual, erro, iter
--- 2-Metodos_abertos/2.3-MetodoSecante/Formula/Secante.py
+++ 2-Metodos_abertos/2.3-MetodoSecante/Formula/Secante.py
@@ -1,2 +1,22 @@
 import math
 import numpy as np
+
+
+def iteracao(f, x0, x1):
+    return ((x0*f(x1) - x1*f(x0))/(f(x1) - f(x0)))
+
+def Secante(x0, x1, f ,Tol, maxIter):
+    
+    iter = 1
+    x_atual = iteracao(f,x0,x1)
+    
+    erro = np.abs((x_atual - x0)/x_atual) * 100
+    
+    while (erro > Tol and iter < maxIter):
+        x_anterior = x_atual
+        x_atual = iteracao(f,x_anterior, x_atual)
+        
+        erro = np.abs((x_atual - x_anterior)/x_atual) * 100
+        iter += 1
+        
+    return x_atual, erro, iter
